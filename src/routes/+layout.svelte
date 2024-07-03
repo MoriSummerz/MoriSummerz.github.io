fdda955d8eabdd9c447115369eabd4b9b58a6382
morisummer <timofeevnikita111@gmail.com>
2024-07-03 22:21:28 +0300

    Fix formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,76 +1,72 @@
 <script lang="ts">
-    import {page} from '$app/stores';
+	import { page } from '$app/stores';
 
-    export const ssr = false;
+	export const ssr = false;
 
-    const links = [
-        {name: 'Portfolio', href: '/'},
-        {name: 'Comic', href: '/comic'}
-    ];
-    console.log($page.url.pathname);
+	const links = [
+		{ name: 'Portfolio', href: '/' },
+		{ name: 'Comic', href: '/comic' }
+	];
+	console.log($page.url.pathname);
 </script>
 
 <header>
-    {#each links as link}
-        <button
-                on:click={() => window.location.href = link.href}
-                class:selected={$page.url.pathname === link.href}
-        >{link.name}</button>
-    {/each}
+	{#each links as link}
+		<button
+			on:click={() => (window.location.href = link.href)}
+			class:selected={$page.url.pathname === link.href}>{link.name}</button
+		>
+	{/each}
 </header>
 <slot></slot>
-<footer>
-    © 2024 morisummer
-</footer>
-
+<footer>© 2024 morisummer</footer>
 
 <style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		width: 100%;
+		height: 60px;
+		background: rgba(255, 255, 255, 0);
+		padding: 0 20px;
+		box-sizing: border-box;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
 
-    header {
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        width: 100%;
-        height: 60px;
-        background: rgba(255, 255, 255, 0);
-        padding: 0 20px;
-        box-sizing: border-box;
-        position: fixed;
-        top: 0;
-        left: 0;
-    }
+	header button {
+		background: rgba(255, 255, 255, 0);
+		color: white;
+		font-size: 16px;
+		margin-right: 15px;
+		cursor: pointer;
+		transition: color 0.3s;
+		border: 1px solid white;
+		border-radius: 5px;
+		padding: 10px 20px;
+	}
 
-    header button {
-        background: rgba(255, 255, 255, 0);
-        color: white;
-        font-size: 16px;
-        margin-right: 15px;
-        cursor: pointer;
-        transition: color 0.3s;
-        border: 1px solid white;
-        border-radius: 5px;
-        padding: 10px 20px;
-    }
+	header button:hover {
+		color: #ccc;
+	}
 
-    header button:hover {
-        color: #ccc;
-    }
+	header button.selected {
+		background-color: rgba(255, 255, 255, 0.1);
+	}
 
-    header button.selected {
-        background-color: rgba(255, 255, 255, 0.1);
-    }
-
-    footer {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 60px;
-        background: rgba(255, 255, 255, 0);
-        color: white;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        opacity: 50%;
-    }
+	footer {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 60px;
+		background: rgba(255, 255, 255, 0);
+		color: white;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		opacity: 50%;
+	}
 </style>
